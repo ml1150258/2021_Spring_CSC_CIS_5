2b0e293d9f660e42691b742d25b843e369dae131
Mark Lehr <mark.lehr@rcc.edu>
2021-02-18 11:27:06 -0800

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

---
+++ Class/CppTemplate/main.cpp
@@ -0,0 +1,34 @@
+/*
+    Author: Dr. Mark E. Lehr
+    Date:   February 18th, 2021  11:12am
+    Purpose:C++ Template to be used in all programs
+    Version:First and Last
+ */
+
+//System Libraries
+#include <iostream>    //Input/Output Library
+using namespace std;   //Library Name-space
+
+//User Libraries
+
+//Global/Universal Constants -- No Global Variables
+//Science, Math, Conversions, Higher Dimensioned constants only
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set the Random number seed
+    
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Process, map inputs to outputs
+    
+    //Display your initial conditions as well as outputs.
+    
+    //Exit stage right
+    return 0;
+}
+
